815700c09c341a14902e26ca080a38a330e83c5a
tfgirlhyl <1449989840@qq.com>
2019-10-29 02:14:27 -0700

    Enter

---
+++ lb1314.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+    printf("TEMPTURE\n")
+{   int celsius , fahr ;
+    int lower ,upper, step;
+
+    lower = 0;
+    upper = 100;
+    step = 5;
+
+    celsius = lower;
+    print("celsius\tfahr\n");
+    while (celsius <= upper){
+        fahr = 9*celsius/5+32;
+        printf("%d\t%d\n", celsius,fahr);
+        celsius = celsius + step;
+        
+    }
+}
