c58edb065425625c2884920cbddd34516fbbfd5a
tfgirlhyl <1449989840@qq.com>
2019-11-11 23:45:41 -0800

    added ex.c file

---
+++ ex.c
@@ -0,0 +1,13 @@
+#include<stadio.h>
+int main()
+{
+    unsigned int a=1;
+    unsigned int b=1;
+    unsigned int c=0-1;
+    while(a!=0){
+        a=a<<1;
+        b=b+1;
+    }
+    printf("a=%d\n",c);
+    printf("%d",b);
+}
